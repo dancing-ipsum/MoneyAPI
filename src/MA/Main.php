64058391d8ec4b33efc1589382f8b9fb9325bdb5
SavionLegendZzz <saviondeavault@gmail.com>
2015-10-19 21:39:09 -0400

    v1.1_Build#1
    Added "EXP"

--- src/MA/Main.php
+++ src/MA/Main.php
@@ -20,13 +20,14 @@ $this->getServer()->getPluginManager()->registerEvents($this,$this);
 		 @mkdir($this->getDataFolder());
 @mkdir($this->getDataFolder()."Players/");	
 }
-
+////////////////MONEY////////////////
 public function addMoney(Player $p, $amount){
 				$ign = $p->getName();
 			$this->PlayerFile = new Config($this->getDataFolder()."Players/".$ign.".yml", Config::YAML);
 		$this->PlayerFile->set("Money",$amount);
 	$this->PlayerFile->save();
 }
+
 public function takeMoney(Player $p, $amount){
 				$ign = $p->getName();
 			$this->PlayerFile = new Config($this->getDataFolder()."Players/".$ign.".yml", Config::YAML);
@@ -41,6 +42,27 @@ public function getMoney(Player $p){
 			$this->PlayerFile = new Config($this->getDataFolder()."Players/".$ign.".yml", Config::YAML);
 		//someone want to help?
 }
+////////////////MONEY////////////////
+
+#BREAK
+
+////////////////EXP////////////////
+public function addEXP(Player $p, $amount){
+				$ign = $p->getName();
+			$this->PlayerFile = new Config($this->getDataFolder()."Players/".$ign.".yml", Config::YAML);
+		$this->PlayerFile->set("EXP",$amount);
+	$this->PlayerFile->save();
+} 
+
+public function takeEXP(Player $p, $amount){
+				$ign = $p->getName();
+			$this->PlayerFile = new Config($this->getDataFolder()."Players/".$ign.".yml", Config::YAML);
+$m = $this->PlayerFile->get("EXP");
+                $a = $m - $amount;
+                $this->PlayerFile->set("EXP", $a);
+	$this->PlayerFile->save();
+}
+////////////////EXP////////////////
 public function onPlayerLogin(PlayerPreLoginEvent $event){
         $ign = $event->getPlayer()->getName();
         $player = $event->getPlayer();
@@ -48,6 +70,7 @@ public function onPlayerLogin(PlayerPreLoginEvent $event){
         if(!file_exists($file)){
                 $this->PlayerFile = new Config($this->getDataFolder()."Players/".$ign.".yml", Config::YAML);
                 		$this->PlayerFile->set("Money",0);
+                		 		$this->PlayerFile->set("EXP",0);
                 $this->PlayerFile->save();
             }
         }
