e833a3fedfc82ea814533e5cbdd3945a84611ce7
SavionLegendZzz <saviondeavault@gmail.com>
2015-10-19 20:39:27 -0400

    Update Main.php

--- src/MA/Main.php
+++ src/MA/Main.php
@@ -1,40 +1,41 @@
-
 <?php
 
 namespace MA;
 
+use pocketmine\utils\Config;
 use pocketmine\Server;
 use pocketmine\Player;
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\event\player\PlayerPreLoginEvent;
 
 class Main extends PluginBase implements Listener{
 
-public function onLoad(){
-@mkdir($this->getDataFolder()."Players/");
-$this->getServer()->getLogger()->info("[MoneyAPI]Loaded!");
-}
+
 
 public function onEnable(){
 $this->getServer()->getLogger()->info("[MoneyAPI]Loaded!");
+
+$this->getServer()->getPluginManager()->registerEvents($this,$this);
+		 @mkdir($this->getDataFolder());
+@mkdir($this->getDataFolder()."Players/");	
 }
 
-public function giveMoney(Player $p, $amount){
+public function addMoney(Player $p, $amount){
 				$ign = $p->getName();
 			$this->PlayerFile = new Config($this->getDataFolder()."Players/".$ign.".yml", Config::YAML);
 		$this->PlayerFile->set("Money",$amount);
 	$this->PlayerFile->save();
 }
+public function onPlayerLogin(PlayerPreLoginEvent $event){
+        $ign = $event->getPlayer()->getName();
+        $player = $event->getPlayer();
+        $file = ($this->getDataFolder()."Players/".$ign.".yml");  
+            if(!file_exists($file)){
+                $this->PlayerFile = new Config($this->getDataFolder()."Players/".$ign.".yml", Config::YAML);
+                		$this->PlayerFile->set("Money",0);
+                $this->PlayerFile->save();
+            }
+        }
 
-public function onLogin(PlayerLoginEvent $event){
-$ign = $event->getPlayer()->getName();
-$player = $event->getPlayer();
-$file = ($this->getDataFolder()."Players/".$ign.".yml");  
-if(!file_exists($file)){
-$this->PlayerFile = new Config($this->getDataFolder()."Players/".$ign.".yml", Config::YAML);
-$this->PlayerFile->set("Money","0");
-$this->PlayerFile->save();
-		}
-	}
 }
